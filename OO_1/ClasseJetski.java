8a8932274265b2a29ab1274b286ef8bbd2d9c248
Kanamy Stewart <80801097+KanamyStewart@users.noreply.github.com>
2021-10-08 21:47:54 -0300

    Add files via upload

---
+++ OO_1/ClasseJetski.java
@@ -0,0 +1,49 @@
+public class ClasseJetski {
+    public static void main(String[] args) {
+        Jetski novoJetski = new Jetski();
+        novoJetski.capacidadeVeiculo = 3;
+        novoJetski.comprimento = 2.9;
+        novoJetski.largura = 1.3;
+        novoJetski.pesoSeco = 80;
+        novoJetski.capacidadeCombustivel = 80;
+        novoJetski.cascoMaterial = "fibra";
+        novoJetski.tipoDoMotor = "Traseiro";
+        novoJetski.gasolinaRecomendada = "Aditivada";
+        novoJetski.potencia = 300;
+        novoJetski.cilindrada = 1630;
+        novoJetski.profundimetro = false;
+        novoJetski.assentoBiPartido = true;
+        novoJetski.assentoErgolock = true;
+        novoJetski.controleDePartida = true;
+        novoJetski.chaveCodificada = true;
+        novoJetski.compartimentoParaCelulares = true;
+        novoJetski.sistemaDeAudio = false;
+        novoJetski.espelhoRetrovisores = true;
+        novoJetski.portaUsb = false;
+        novoJetski.displayDigital = true;
+        novoJetski.colunaDeDirecaoAjustavel = true;
+        
+        System.out.println(novoJetski.capacidadeVeiculo + ": Pessoas");
+        System.out.println(novoJetski.comprimento + ": Metros de comprimento");
+        System.out.println(novoJetski.largura + ": Metros de largura");
+        System.out.println(novoJetski.pesoSeco + ": Peso do veiculo seco");
+        System.out.println(novoJetski.capacidadeCombustivel + ": Capacidade de combustivel");
+        System.out.println(novoJetski.cascoMaterial + ": Material");
+        System.out.println(novoJetski.tipoDoMotor + ": Tipo do motor");
+        System.out.println(novoJetski.gasolinaRecomendada + ": Gasolina recomendada");
+        System.out.println(novoJetski.potencia + ": HP");
+        System.out.println(novoJetski.cilindrada + ": Cilindradas");
+        System.out.println(novoJetski.profundimetro + ": Profuncimetro");
+        System.out.println(novoJetski.assentoBiPartido + ": Assento Bipartido");
+        System.out.println(novoJetski.assentoErgolock + ": Assento Ergolock");
+        System.out.println(novoJetski.controleDePartida + ": Controle de Partida");
+        System.out.println(novoJetski.chaveCodificada + ": Chave codificada");
+        System.out.println(novoJetski.compartimentoParaCelulares + ": Compartimento para Celulares");
+        System.out.println(novoJetski.sistemaDeAudio + ": Sistema de Audio");
+        System.out.println(novoJetski.espelhoRetrovisores + ": Espelho Retrovisor");
+        System.out.println(novoJetski.portaUsb + ": Porta USB");
+        System.out.println(novoJetski.displayDigital + ": Display Digital");
+        System.out.println(novoJetski.colunaDeDirecaoAjustavel + ": Coluna de Direção Ajustavel");
+
+    }
+}
---
+++ OO_1/Jetski.java
@@ -0,0 +1,23 @@
+public class Jetski {
+    int capacidadeVeiculo;
+    double comprimento;
+    double largura;
+    double pesoSeco;
+    double capacidadeCombustivel;
+    String cascoMaterial;
+    String tipoDoMotor;
+    String gasolinaRecomendada ;
+    int potencia;
+    int cilindrada;
+    boolean profundimetro;
+    boolean assentoErgolock;
+    boolean assentoBiPartido;
+    boolean controleDePartida;
+    boolean chaveCodificada;
+    boolean compartimentoParaCelulares;
+    boolean sistemaDeAudio;
+    boolean espelhoRetrovisores;
+    boolean portaUsb;
+    boolean displayDigital;
+    boolean colunaDeDirecaoAjustavel;
+}
