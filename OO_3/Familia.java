fcd3ab322b6273518649f04c0224db4da574e13c
Kanamy Stewart <80801097+KanamyStewart@users.noreply.github.com>
2021-10-18 22:00:53 -0300

    Add files via upload

---
+++ OO_3/Familia.java
@@ -0,0 +1,12 @@
+public class Familia {
+    public static void main(String[] args) {
+    Tio tioZeca = new Tio("Zeca", "cerveja", "Malandro é malandro mané é mane");
+    Tio tioArlindo = new Tio("Arlindo", "picanhazinha", "salve zé meningite");
+    Tio tioVerdinho = new Tio("Bob marley", "Erva da paz", "Snoop lyon");
+    
+    Sobrinho sobrinhoUm = new Sobrinho("João", "Pia do dinhano");
+    Sobrinho sobrinhoDois = new Sobrinho("Mariazinha", "nem te conto");
+
+
+    }
+}
